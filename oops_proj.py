0f2de1290bae3d209b896c8d735a2edf71917c80
AdoniRasheed <rasheedadoni132@gmail.com>
2026-01-30 18:02:45 +0530

    Signup and Signin Function Added

--- oops_proj.py
+++ oops_proj.py
@@ -15,14 +15,41 @@ class chatbook:
                            5. Press anyother key to exit""")
         
         if user_input == '1':
-            pass
+            self.Signup()
         elif user_input == '2':
-            pass
+            self.Signin()
         elif user_input == '3':
             pass
         elif user_input == '4':
             pass
         else:
             exit()
+
+
+    def Signup(self):
+        email = input('Enter your email id')
+        password = input('Enter your password')
+        self.username = email
+        self.password = password
+        print('You have signedup successfuly')
+        print('\n')
+        self.menu()
+
+    def Signin(self):
+        if self.username == "" and self.password == "":
+            print('First signup by selecting 1 option in the meain menu')
+        else:
+            username = input('Enter your email id/usename')
+            password = input('Enter your password')
+            if self.username == username and self.password == password:
+                print('You have logged in successfuly')
+                self.loggedin = True
+            else:
+                print('Kindly input correct credentials....')
+        print('\n')
+        self.menu()
+
+
+    
 
 obj = chatbook()
